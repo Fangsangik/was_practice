90aec2a08fbe0c9e20a76d20f7045b84e8ce2b17
hwang <hwangsangik@gmail.com>
2024-07-09 22:48:06 +0900

    "HttpServlet"

--- src/main/java/hello/was_practice/servletcalculator/CalculatorServletV3.java
+++ src/main/java/hello/was_practice/servletcalculator/CalculatorServletV3.java
@@ -28,4 +28,15 @@ public class CalculatorServletV3 extends HttpServlet {
         System.out.println("rst = " + rst);
     }
     //HttpServlet ->  get 요청일때 Post, delete 요청마다 메소드를 구현 해주면 된다.
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int param1 = Integer.parseInt(request.getParameter("param1"));
+        String operator = request.getParameter("operator");
+        int param2 = Integer.parseInt(request.getParameter("param2"));
+
+        int rst = Calculator.calculate(new PositiveNumber(param1), operator, new PositiveNumber(param2));
+        System.out.println("rst = " + rst);
+    }
+    //HttpServlet ->  get 요청일때 Post, delete 요청마다 메소드를 구현 해주면 된다.
 }
