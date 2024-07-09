45187257f990448e7581912b0bd2a0d82ff7d5c5
hwang <hwangsangik@gmail.com>
2024-07-09 21:58:50 +0900

    "singleThread"

--- src/main/java/hello/was_practice/CustomWebApplicationServer.java
+++ src/main/java/hello/was_practice/CustomWebApplicationServer.java
@@ -2,6 +2,7 @@ package hello.was_practice;
 
 import hello.was_practice.calculator.Calculator;
 import hello.was_practice.calculator.PositiveNumber;
+import hello.was_practice.calculator.handler.ClientRequestHandler;
 import hello.was_practice.query.QueryStrings;
 import hello.was_practice.request.HttpRequest;
 import hello.was_practice.response.HttpResponse;
@@ -24,6 +25,9 @@ public class CustomWebApplicationServer {
     }
 
     //톰켓을 한번 만들어 봄
+    //client 요청이 들어왔을때 해당 작업이 mainthread에서 실행
+    //if block 걸릴시 다음 실행 요청에 상당한 지연 상황 발생
+    //따라서 별도 Thread에서 진행 할 수 있도록 필요
     public void start() throws IOException {
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             log.info("CustomWebApplication started {]", port);
@@ -34,47 +38,49 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 log.info("CustomWebApplication client connected");
 
+                //client 마다 별도 스레드 수행
+                new Thread(new ClientRequestHandler(clientSocket)).start();
+
                 /**
                  * step1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
                  */
-
-                try {
-                    InputStream in = clientSocket.getInputStream();
-                    OutputStream out = clientSocket.getOutputStream();
-                    //InputStream -> reader로 변경하고 싶었음  (line by line)
-                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
-                    DataOutputStream dos = new DataOutputStream(out);
-
-
-                    //연결된 BufferedReader를 전달
-                    HttpRequest httpRequest = new HttpRequest(br);
-
-                    //GET 요청 & path가 calculate와 일치하면 queryStrings 가져옴
-                    if (httpRequest.isGetRequest() && httpRequest.mathPath("/calculate")) {
-                       QueryStrings queryStrings = httpRequest.getQueryStrings();
-
-                       int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
-                       String operator = queryStrings.getValue("operator");
-                       int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
-
-                        int rst = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
-                        byte[] body = String.valueOf(rst).getBytes();
-
-                        HttpResponse response = new HttpResponse(dos);
-                        response.response200Header("application/json", body.length);
-                        response.responseBody(body);
-                    }
-
-                    //protocal 보여주는 부분 필요 없어짐
+//
+//                try {
+//                    InputStream in = clientSocket.getInputStream();
+//                    OutputStream out = clientSocket.getOutputStream();
+//                    //InputStream -> reader로 변경하고 싶었음  (line by line)
+//                    BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
 //                    DataOutputStream dos = new DataOutputStream(out);
 //
-//                    String line;
-//                    while ((line = br.readLine()) != "") {
-//                        System.out.println(line);
+//
+//                    //연결된 BufferedReader를 전달
+//                    HttpRequest httpRequest = new HttpRequest(br);
+//
+//                    //GET 요청 & path가 calculate와 일치하면 queryStrings 가져옴
+//                    if (httpRequest.isGetRequest() && httpRequest.mathPath("/calculate")) {
+//                       QueryStrings queryStrings = httpRequest.getQueryStrings();
+//
+//                       int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+//                       String operator = queryStrings.getValue("operator");
+//                       int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+//
+//                        int rst = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+//                        byte[] body = String.valueOf(rst).getBytes();
+//
+//                        HttpResponse response = new HttpResponse(dos);
+//                        response.response200Header("application/json", body.length);
+//                        response.responseBody(body);
 //                    }
-                } catch (Exception e){
-                    e.printStackTrace();
-                }
+//
+//                    //protocal 보여주는 부분 필요 없어짐
+//                   DataOutputStream dos = new DataOutputStream(out);
+//
+//                   String line;
+//                   while ((line = br.readLine()) != "") {
+//                       System.out.println(line);
+//                   }
+//                } catch (Exception e){
+//                    e.printStackTrace();
             }
         }
     }
---
+++ src/main/java/hello/was_practice/calculator/handler/ClientRequestHandler.java
@@ -0,0 +1,60 @@
+package hello.was_practice.calculator.handler;
+
+import hello.was_practice.calculator.Calculator;
+import hello.was_practice.calculator.PositiveNumber;
+import hello.was_practice.query.QueryStrings;
+import hello.was_practice.request.HttpRequest;
+import hello.was_practice.response.HttpResponse;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
+@RequiredArgsConstructor
+public class ClientRequestHandler implements Runnable{
+
+    //사용자가 요청 할때마다 thread를 생성 한다면 메모리 할당, -> 성능이 매우 떨어지는 형태
+    //CPU, contextSwitching 형태가 급격히 증가 -> 최악의 경우 서버 터짐
+
+    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+    private final Socket clientSocket;
+    @Override
+    public void run() {
+        /**
+         * step2 - 시용자 요청이 들어올 때마다 Thread를 세로 생성해서, 요청을 처리하도록 한다.
+         */
+        logger.info("{ClientRequestHandler} new client {} started", Thread.currentThread().getName()); //현재 실행되는 Thread 이름
+        try {
+            //Thread 별로 input, output Thread 얻는다.
+            InputStream in = clientSocket.getInputStream();
+            OutputStream out = clientSocket.getOutputStream();
+            //InputStream -> reader로 변경하고 싶었음  (line by line)
+            BufferedReader br = new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8));
+            DataOutputStream dos = new DataOutputStream(out);
+
+            //연결된 BufferedReader를 전달
+            HttpRequest httpRequest = new HttpRequest(br);
+
+            //GET 요청 & path가 calculate와 일치하면 queryStrings 가져옴
+            if (httpRequest.isGetRequest() && httpRequest.mathPath("/calculate")) {
+                QueryStrings queryStrings = httpRequest.getQueryStrings();
+
+                int operand1 = Integer.parseInt(queryStrings.getValue("operand1"));
+                String operator = queryStrings.getValue("operator");
+                int operand2 = Integer.parseInt(queryStrings.getValue("operand2"));
+
+                int rst = Calculator.calculate(new PositiveNumber(operand1), operator, new PositiveNumber(operand2));
+                byte[] body = String.valueOf(rst).getBytes();
+
+                HttpResponse response = new HttpResponse(dos);
+                response.response200Header("application/json", body.length);
+                response.responseBody(body);
+            }
+        } catch (IOException e) {
+            logger.error(e.getMessage());
+        }
+    }
+}
